0f57d285738625327baca7e482fd3e382736315e
Alice Kile <buoyantair@gmail.com>
2024-01-06 23:52:25 +0100

    dweight program

--- LearningC/dweight.c
+++ LearningC/dweight.c
@@ -1,18 +1,33 @@
+#include <stdio.h>
+
 #include "dweight.h"
+
+#define INCHES_PER_POUND 166
 
 /* Computes the dimentional weight of a 12" x 10" x 8" box. */
 
 void dweight(void)
 {
 	int
-		height = 8,
-		length = 12,
-		width = 10,
-		weight = 10,
-		volume = height * length * width;
+		height,
+		length,
+		width,
+		weight,
+		volume;
 
+	printf("Enter height of the box: ");
+	scanf_s("%d", &height);
+
+	printf("Enter length of the box: ");
+	scanf_s("%d", &length);
+
+	printf("Enter width of the box: ");
+	scanf_s("%d", &width);
+
+	volume = height * length * width;
+	weight = (volume + INCHES_PER_POUND - 1) / INCHES_PER_POUND;
 
 	printf("Dimensions: %dx%dx%d\n", length, width, height);
 	printf("Volume (cubic inches): %d\n", volume);
-	printf("Dimentional weight (pounds): %d\n", (volume + 165) / 166);
+	printf("Dimentional weight (pounds): %d\n", weight);
 }
