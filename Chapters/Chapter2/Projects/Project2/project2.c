fe393e5a3999a3003868fdf1da1a19d578a1aa2d
Alice Kile <buoyantair@gmail.com>
2024-01-07 18:32:29 +0100

    vol of sphere proj

---
+++ Chapters/Chapter2/Projects/Project2/project2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+#define _USE_MATH_DEFINES
+
+#include <math.h>
+
+
+
+float volOfSphere()
+{
+	int radius;
+	float factor = 4.0f / 3.0f;
+
+	printf("Enter the radius of Sphere: ");
+	scanf_s("%d", &radius);
+
+	return factor * M_PI * radius * radius * radius;
+}
+
+int main(void)
+{
+	printf("Volume of Sphere is: %f", volOfSphere());
+}
