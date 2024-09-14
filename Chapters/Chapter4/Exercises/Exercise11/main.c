807669263459d776c6c3077ecefb83c489c505e7
Alice Kile <buoyantair@protonmail.com>
2024-09-14 20:15:24 +0200

    Add files

---
+++ Chapters/Chapter4/Exercises/Exercise11/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int i = 1;
+	printf("%d ", i++ - 1);
+	printf("%d\n", i);
+
+	i = 10; int j = 5;
+	printf("%d ", i++ - ++j);
+	printf("%d %d\n", i, j);
+
+	i = 7; j = 8;
+	printf("%d ", i++ - --j);
+	printf("%d %d\n", i, j);
+
+	i = 3; j = 4; int k = 5;
+	printf("%d ", i++ - j++ + --k);
+	printf("%d %d %d", i, j, k);
+}
--- LearningC/main.c
+++ LearningC/main.c
@@ -2,16 +2,6 @@
 
 int main(void)
 {
-
-	const int* arr = (const int[]){ 0, 1, 2, 3, 4 };
-
-	printf("Please enter an array index:\n");
-
-	int index;
-
-	scanf("%d", &index);
-
-	int element = arr[index];
-
-	printf("The value of the element at index %d is: %d", index, element);
+	int i = 1;
+	printf("%d", ++i);
 }
