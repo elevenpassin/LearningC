f7cd7ab354c1fdf047d872a974d1bdc9496a9009
Alice Kile <buoyantair@protonmail.com>
2025-02-23 17:48:00 +0100

    Update

---
+++ Chapters/Chapter5/Exercises/Exercise5.6/main.c
@@ -0,0 +1,8 @@
+/*
+
+	if (n == 1-10)
+		printf("n is between 1 and 10\n");
+
+	Statement is legal but the implementation is incorrect, if for example n = 5, then the final expression to be evaluated by the equality operator is (5 == -9) which is obviously going to return 0.
+*/
+
