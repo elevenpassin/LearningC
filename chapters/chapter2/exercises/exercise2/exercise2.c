f51587d2cf8d87fde0b1da5f79e0750b7fe17419
Alice Kile <buoyantair@protonmail.com>
2025-05-08 11:21:58 +0200

    migrate to linux

---
+++ chapters/chapter2/exercises/exercise2/exercise2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("Parkinson's Law:\nWork expands so as to ");
+	printf("fill the time\n");
+	printf("available for it's completion.\n");
+
+	return 0;
+}
---
+++ chapters/chapter2/exercises/exercise4/exercise4.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+int main(void)
+{
+	int x, y, z;
+	float a, b, c;
+
+	printf("integers: %d, %d, %d\n", x, y, z);
+	printf("floats: %f, %f, %f\n", a, b, c);
+
+	return 0;
+}
